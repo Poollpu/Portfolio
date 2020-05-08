3a629a5bbc5811b007f9330bdaf293b5fc65f4d8
Poollpu <64378083+Poollpu@users.noreply.github.com>
2020-05-08 16:00:24 -0500

    Add files via upload

---
+++ Actividad semana 5/js/codigo-redes.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+function ver(n)
+{
+	
+	
+if(n==0){
+document.getElementById("1").innerText="Red de pesca";
+	
+document.getElementById("imgmar").src="../imagenes/img1.jpg";
+	/* ENLAZE SECTION (titulo , imgmar) ENLAZADA PARA TEXTO E IMAGEN*/
+}
+	
+else if(n==1){
+document.getElementById("2").innerText="Ciudad de Caral";
+	
+document.getElementById("imgmar").src="../imagenes/img2.jpg";
+	/* ENLAZE SECTION (titulo , imgmar) ENLAZADA PARA TEXTO E IMAGEN*/
+}
+	
+else{
+document.getElementById("3").innerText="Ciudadela Kuelap";
+	
+document.getElementById("imgmar").src="../imagenes/img3.jpg";
+	/* ENLAZE SECTION(titulo , imgmar) ENLAZADA PARA TEXTO E IMAGEN*/
+}
+	
+	
+	
+}
