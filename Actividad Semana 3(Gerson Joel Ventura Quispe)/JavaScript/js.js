eaa031c9371455cbe2aafcf22bb480c00e96819e
Poollpu <64378083+Poollpu@users.noreply.github.com>
2020-05-08 22:07:21 -0500

    Add files via upload

---
+++ Actividad Semana 3(Gerson Joel Ventura Quispe)/JavaScript/js.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+function ver(n)
+{
+	
+	
+if(n==0){
+document.getElementById("titulo").innerText="Machu Picchu";
+	
+document.getElementById("imgmar").src="../imagenes/MP.jpg";
+	/* ENLAZE SECTION (titulo , imgmar) ENLAZADA PARA TEXTO E IMAGEN*/
+}
+	
+else if(n==1){
+document.getElementById("titulo").innerText="Ciudad de Caral";
+	
+document.getElementById("imgmar").src="../imagenes/caral.jpg";
+	/* ENLAZE SECTION (titulo , imgmar) ENLAZADA PARA TEXTO E IMAGEN*/
+}
+	
+else{
+document.getElementById("titulo").innerText="Ciudadela Kuelap";
+	
+document.getElementById("imgmar").src="../imagenes/kuelap.jpg";
+	/* ENLAZE SECTION(titulo , imgmar) ENLAZADA PARA TEXTO E IMAGEN*/
+}
+	
+	
+	
+}
